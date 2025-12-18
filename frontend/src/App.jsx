5ecc705f641b42c7eee1f5c83f38bf1f2231bd94
Santhosh <santhoshkumar.s9c@gmail.com>
2025-12-18 11:30:29 +0530

    Use HashRouter to fix 404 on Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import QRPage from "./QRPage";
 import SessionPage from "./SessionPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<QRPage />} />
         <Route path="/session/:id" element={<SessionPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
